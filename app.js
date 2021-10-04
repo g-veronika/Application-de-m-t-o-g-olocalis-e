c2684f11421e802a1621f6e502cdcea1e028e9c3
g-veronika <g.veronika2021@gmail.com>
2021-10-04 18:02:59 +0200

    bug fixed(bootstrap)

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
 let villeChoisie;
 
 if("geolocation" in navigator) {
+  navigator.geolocation.watchPosition((position) => {
+    
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat='
+        + position.coords.latitude + '&lon='
+        + position.coords.longitude + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+    
+    let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+    requete.open('GET', url); // Nous récupérons juste des données
+    requete.responseType = 'json'; // Nous attendons du JSON
+    requete.send(); // Nous envoyons notre requête
 
-    navigator.geolocation.watchPosition((position)=> {
-
-        const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
-        console.log(url)
-
-        let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
-        requete.open('GET', url); // Nous récupérons juste des données
-        requete.responseType = 'json'; // Nous attendons du JSON
-        requete.send(); // Nous envoyons notre requête
-      
-        // Dès qu'on reçoit une réponse, cette fonction est executée
-        requete.onload = function() {
-          if (requete.readyState === XMLHttpRequest.DONE) {
-            if (requete.status === 200) {
-              let reponse = requete.response;
-              // console.log(reponse);
-              let temperature = reponse.main.temp;
-              let ville       = reponse.name;
-              // console.log(temperature);
-              document.querySelector('#temperature_label').textContent = temperature;
-              document.querySelector('#ville').textContent = ville;
-            }
-            else {
-              alert('Un problème est intervenu, merci de revenir plus tard.');
-            }
-          }
+    // Dès qu'on reçoit une réponse, cette fonction est executée
+    requete.onload = function() {
+      if (requete.readyState === XMLHttpRequest.DONE) {
+        if (requete.status === 200) {
+          let reponse = requete.response;
+          // console.log(reponse);
+          let temperature = reponse.main.temp;
+          let ville       = reponse.name;
+          // console.log(temperature);
+          document.querySelector('#temperature_label').textContent = temperature;
+          document.querySelector('#ville').textContent = ville;
         }
-        
-    }, erreur, options);
-
-    var options = {
-        enableHighAccuracy: true
+        else {
+          alert('Un problème est intervenu, merci de revenir plus tard.');
+        }
+      }
     }
-
-} else {
-    villeChoisie = "Paris";
-    recevoirTemperature(villeChoisie);
-};
-
+  }, erreur, options);
+  
+  var options = {
+    enableHighAccuracy: true
+  }
+}
+else {
+  villeChoisie = "saint-saulve";
+  recevoirTemperature(villeChoisie);
+}
 
 let changerDeVille = document.querySelector('#changer');
 changerDeVille.addEventListener('click', () => {
@@ -49,35 +47,33 @@ changerDeVille.addEventListener('click', () => {
 });
 
 function erreur() {
-    villeChoisie = 'Paris';
-    recevoirTemperature(villeChoisie);
+  villeChoisie = "Saint-Saulve";
+  recevoirTemperature(villeChoisie);
 }
 
-function recevoirTemperature(position) {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
-        console.log(url)
-        let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
-        requete.open('GET', url); // Nous récupérons juste des données
-        requete.responseType = 'json'; // Nous attendons du JSON
-        requete.send(); // Nous envoyons notre requête
-      
-        // Dès qu'on reçoit une réponse, cette fonction est executée
-        requete.onload = function() {
-          if (requete.readyState === XMLHttpRequest.DONE) {
-            if (requete.status === 200) {
-              let reponse = requete.response;
-              // console.log(reponse);
-              let temperature = reponse.main.temp;
-              let ville       = reponse.name;
-              // console.log(temperature);
-              document.querySelector('#temperature_label').textContent = temperature;
-              document.querySelector('#ville').textContent = ville;
-            }
-            else {
-              alert('Un problème est intervenu, merci de revenir plus tard.');
-            }
-          }
-        }
-    
+function recevoirTemperature(ville) {
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+
+  let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+  requete.open('GET', url); // Nous récupérons juste des données
+  requete.responseType = 'json'; // Nous attendons du JSON
+  requete.send(); // Nous envoyons notre requête
+
+  // Dès qu'on reçoit une réponse, cette fonction est executée
+  requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+      if (requete.status === 200) {
+        let reponse = requete.response;
+        // console.log(reponse);
+        let temperature = reponse.main.temp;
+        let ville       = reponse.name;
+        // console.log(temperature);
+        document.querySelector('#temperature_label').textContent = temperature;
+        document.querySelector('#ville').textContent = ville;
+      }
+      else {
+        alert('Un problème est intervenu, merci de revenir plus tard.');
+      }
     }
- 
+  }
+}
